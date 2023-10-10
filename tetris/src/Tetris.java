517328c85e5260e188473451f252b959105020ed
Mehmet Uğur Yıldırım <109519208+muguryildirim@users.noreply.github.com>
2023-10-10 20:50:28 +0300

    Add files via upload

---
+++ tetris/src/Tetris.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Tetris extends JFrame {
+
+    private JLabel statusBar;
+
+    public Tetris() {
+        initUI();
+    }
+
+    private void initUI() {
+        statusBar = new JLabel("0");
+        add(statusBar, BorderLayout.SOUTH);
+
+        var board = new Board(this);
+        add(board);
+        board.start();
+
+        setTitle("Tetris");
+        setSize(200, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    JLabel getStatusBar() {
+        return statusBar;
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            var game = new Tetris();
+            game.setVisible(true);
+        });
+    }
+}
